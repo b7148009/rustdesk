c0fa78153b6ca1f4c6f8a4e829ae33a2cec83207
b7148009 <b7148009@126.com>
2024-12-17 20:25:22 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["1n4nz0.job99.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "BDphTm7drHkF8b4GwSeR";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.10.10.2"];
+pub const PUBLIC_RS_PUB_KEY: &str = "yNFhit3Jjseoq6osA1azaxId9hVVEdzCc7yMvBKfyb4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
